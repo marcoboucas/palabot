3d3eb78c5801862fd2b19a6a9405370d71ad6b26
paladorn <marco.boucas@hotmail.fr>
2020-03-25 22:31:29 +0100

    Adding features last day

--- trainingTF/main.py
+++ trainingTF/main.py
@@ -1,6 +1,9 @@
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Embedding, LSTM
+from tensorflow.keras.optimizers import RMSprop
 import pandas as pd
-import tensorflow
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 
@@ -52,6 +55,7 @@ for sentence in data['text']:
     X_before.append(sentence)
 
 vocab = list(vocab)
+len_vocab = len(list(vocab))+1
 text_to_index = {}
 for i, word in enumerate(vocab):
     text_to_index[word] = i+1
@@ -62,3 +66,53 @@ X = np.array(list(map(transform, X_before)))
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+
+# Creation of the model
+
+
+input = Input(
+    shape=(max_len,)
+)
+
+embedding = Embedding(
+    input_dim=len_vocab,
+    output_dim=10,
+    mask_zero=True
+)(input)
+
+lstm = LSTM(
+    units=50
+)(embedding)
+
+hidden = Dense(25)(lstm)
+output = Dense(3, activation="sigmoid")(hidden)
+
+model = Model(inputs=input, outputs=output)
+model.compile(
+    optimizer=RMSprop(learning_rate=0.0001, rho=0.9),
+    loss='categorical_crossentropy',
+    metrics=['accuracy'],
+)
+
+
+history = model.fit(X_train, y_train, epochs=1, batch_size=32, verbose=10,
+                    validation_data=(X_test, y_test), shuffle=True)
+
+# Plot training & validation accuracy values
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
